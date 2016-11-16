697c39cfb6527695bd569fd6af8cf8e491d11183
iotgtkol <1450410326@qq.com>
2016-11-16 21:53:02 +0800

    Create LiuWei-lodash.js

---
+++ LiuWei-lodash.js
@@ -0,0 +1,38 @@
+var LiuWei = {
+	chuck：
+	function(arr, size) {
+		var l = Math.ceil(arr.length / size)
+		var result = new Array(l)
+		for (var i = 0; i < l; i++) {
+			result[i] = []
+		}
+		for (var i = 0; i < arr.length; i++) {
+			result[parseInt(i / size)][i % size] = arr[i]
+		}
+		return result
+	},
+	compact: function(array) {
+		var result = []
+		for (var i = 0; i < array.length; i++) {
+			if (array[i]) {
+				result.push(array[i])
+			}
+		}
+	},
+	concat: function(array, [values]) {
+
+	},
+	difference: function(array, [values]) {
+		var a = []
+		var b = []
+		var result = []
+		for (var i = 0; i < a.length; i++) {
+			if (a[i] == b[i]) {
+				result.push(a[i])
+			}
+		}
+		return result
+	},
+
+
+}
