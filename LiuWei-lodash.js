13b3b235ed89f01c505409882056b1029c53e6eb
iotgtkol <1450410326@qq.com>
2016-11-17 22:34:40 +0800

    Update LiuWei-lodash.js

--- LiuWei-lodash.js
+++ LiuWei-lodash.js
@@ -55,7 +55,18 @@ var LiuWei = {
 			result[i] = array[i]
 		}
 		return result
-	}
+	},
+	intersection: function(arr1, arr2) {
+		var result = []
+		for (i = 0; i < arr1.length; i++) {
+			for (var j = 0; j < arr2.length; j++) {
+				if (arr1[i] == arr2[j]) {
+					result.push(arr1[i])
+				}
+			}
+		}
+		return result
+	},
 
 
 }
