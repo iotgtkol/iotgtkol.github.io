7671ff120205ab42b8c50054f8af2ea03743e23e
iotgtkol <1450410326@qq.com>
2016-11-17 19:28:59 +0800

    Update LiuWei-lodash.js

--- LiuWei-lodash.js
+++ LiuWei-lodash.js
@@ -49,6 +49,13 @@ var LiuWei = {
 		}
 		return array
 	},
+	initial: function(array) {
+		var result = []
+		for (var i = 0; i < array.length - 1; i++) {
+			result[i] = array[i]
+		}
+		return result
+	}
 
 
 }
